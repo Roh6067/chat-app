e6104587c2c8429663fba1120a8fd5db1b0b651b
Roh6067 <rohanahirwar457@gmail.com>
2025-12-26 16:50:14 +0530

    signup page refactor

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-// load env config
-dotenv.config();
-
-const connectDB = async () => {
+ export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log("✅ MongoDB Connected");
+    const{ MONGODB_URI } = process.env;
+    if(!MONGODB_URI) throw new Error("MONGODB_URI is not defined in environment variables");
+    const conn =await mongoose.connect(process.env.MONGODB_URI);
+    console.log("✅ MongoDB Connected", conn.connection.host);
+
   } catch (error) {
     console.error(`✖️ MongoDB Error: ${error.message}`);
     process.exit(1);
