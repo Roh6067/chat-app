ad4f280323055f9972a1a5eff41ebfe6acad8a6a
Roh6067 <rohanahirwar457@gmail.com>
2026-01-03 21:52:34 +0530

    update profile chnages done

--- backend/src/controller/auth-controller.js
+++ backend/src/controller/auth-controller.js
@@ -118,8 +118,23 @@ export const logout = async (_, res) => {
 };
 
 export const updateProfile = async (req, res) => {
-  const { fullName, profilePic } = req.body;
-  const userId = req.user.id;
-  console.log("will update later");
-  
-}
+  try {
+    const { profilePic } = req.body;
+    if (!profilePic) return res.status(400).json({ message: "Profile pic is required" });
+
+    const userId = req.user._id;
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadResponse.secure_url },
+      { new: true }
+    );
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log("Error in update profile:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
